95a28db14f316edaa6d53f07f26090ce476af06a
jacktembo <jack@jacktembo>
2022-09-20 12:12:12 +0200

    trying to fix

--- PackageTracking/admin.py
+++ PackageTracking/admin.py
@@ -1,24 +1,24 @@
-from django.contrib import admin
-from .models import KazangSession, Transaction, PricingPlan
-
-@admin.register(KazangSession)
-class KazangSesionAdmin(admin.ModelAdmin):
-    list_display = [
-        'date_time_created', 'session_uuid'
-    ]
-    list_filter = ['date_time_created']
-
-
-@admin.register(Transaction)
-class TransactionAdmin(admin.ModelAdmin):
-    list_display = [
-        'phone_number', 'amount', 'status', 'session_uuid', 'request_reference',
-        'product_id', 'date_time_created'
-    ]
-
-    list_filter = ['status', 'date_time_created']
-
-
-@admin.register(PricingPlan)
-class PricingPlanAdmin(admin.ModelAdmin):
-    list_display = ['tier_name', 'number_of_packages']
+# from django.contrib import admin
+# from .models import KazangSession, Transaction, PricingPlan
+#
+# @admin.register(KazangSession)
+# class KazangSesionAdmin(admin.ModelAdmin):
+#     list_display = [
+#         'date_time_created', 'session_uuid'
+#     ]
+#     list_filter = ['date_time_created']
+#
+#
+# @admin.register(Transaction)
+# class TransactionAdmin(admin.ModelAdmin):
+#     list_display = [
+#         'phone_number', 'amount', 'status', 'session_uuid', 'request_reference',
+#         'product_id', 'date_time_created'
+#     ]
+#
+#     list_filter = ['status', 'date_time_created']
+#
+#
+# @admin.register(PricingPlan)
+# class PricingPlanAdmin(admin.ModelAdmin):
+#     list_display = ['tier_name', 'number_of_packages']
--- general/models.py
+++ general/models.py
@@ -40,47 +40,47 @@ class CourierCompany(models.Model):
         return self.company_name
 
 
-class Vehicle(models.Model):
-    """
-    The actual bus that will be delivering packages.
-    """
-    courier_company = models.ForeignKey(CourierCompany, on_delete=models.CASCADE)
-    vehicle_full_name = models.CharField(max_length=255)
-    departure_time = models.TimeField()
-    transit_time = models.IntegerField(help_text='Number of hours it takes for vehicle to reach the destination.')
-
-    def __str__(self):
-        return self.vehicle_full_name
-
-    class Meta:
-        verbose_name_plural = 'Vehicles'
-
-
-class Package(models.Model):
-    """Package to be tracked"""
-    tracking_number = models.CharField(max_length=255, unique=True, default=calculate_tracking_number(), editable=False)
-    receiver_name = models.CharField(max_length=255)
-    receiver_phone_number = models.CharField(max_length=255)
-    sender_phone_number = models.CharField(max_length=255)
-    delivery_town = models.CharField(max_length=255)
-    starting_town = models.CharField(max_length=255, default='Lusaka')
-    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
-    number_of_packages = models.IntegerField(default=1)
-    price = models.FloatField(default=0.0)
-    departure_date = models.DateField(blank=True, null=True)
-    departure_time = models.TimeField()
-    processed_date_time = models.DateTimeField(auto_now_add=True) # Time package was processed.
-    transit_date_time = models.DateTimeField(blank=True, null=True) # Time vehicle started moving.
-    ready_for_collection_date_time = models.DateTimeField(blank=True, null=True) # Time package becomes ready for collection.
-    collected_date_time = models.DateTimeField(blank=True, null=True) # Time package was collected.
-    processed_status = models.BooleanField(default=True, blank=True, null=True)
-    transit_status = models.BooleanField(default=False, blank=True, null=True)
-    transit_message = models.CharField(max_length=255, default='In transit.')
-    ready_for_collection_status = models.BooleanField(default=False, blank=True, null=True)
-    collected_status = models.BooleanField(default=False, blank=True, null=True)
-    current_coordinates = models.CharField(max_length=255, default="0.0, 0.0")
-    previous_town = models.CharField(max_length=255, default='...')
-
-    def __str__(self):
-        return f"{self.tracking_number} ---> {self.starting_town} - {self.delivery_town}"
-
+# class Vehicle(models.Model):
+#     """
+#     The actual bus that will be delivering packages.
+#     """
+#     courier_company = models.ForeignKey(CourierCompany, on_delete=models.CASCADE)
+#     vehicle_full_name = models.CharField(max_length=255)
+#     departure_time = models.TimeField()
+#     transit_time = models.IntegerField(help_text='Number of hours it takes for vehicle to reach the destination.')
+#
+#     def __str__(self):
+#         return self.vehicle_full_name
+#
+#     class Meta:
+#         verbose_name_plural = 'Vehicles'
+#
+#
+# class Package(models.Model):
+#     """Package to be tracked"""
+#     tracking_number = models.CharField(max_length=255, unique=True, default=calculate_tracking_number(), editable=False)
+#     receiver_name = models.CharField(max_length=255)
+#     receiver_phone_number = models.CharField(max_length=255)
+#     sender_phone_number = models.CharField(max_length=255)
+#     delivery_town = models.CharField(max_length=255)
+#     starting_town = models.CharField(max_length=255, default='Lusaka')
+#     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+#     number_of_packages = models.IntegerField(default=1)
+#     price = models.FloatField(default=0.0)
+#     departure_date = models.DateField(blank=True, null=True)
+#     departure_time = models.TimeField()
+#     processed_date_time = models.DateTimeField(auto_now_add=True) # Time package was processed.
+#     transit_date_time = models.DateTimeField(blank=True, null=True) # Time vehicle started moving.
+#     ready_for_collection_date_time = models.DateTimeField(blank=True, null=True) # Time package becomes ready for collection.
+#     collected_date_time = models.DateTimeField(blank=True, null=True) # Time package was collected.
+#     processed_status = models.BooleanField(default=True, blank=True, null=True)
+#     transit_status = models.BooleanField(default=False, blank=True, null=True)
+#     transit_message = models.CharField(max_length=255, default='In transit.')
+#     ready_for_collection_status = models.BooleanField(default=False, blank=True, null=True)
+#     collected_status = models.BooleanField(default=False, blank=True, null=True)
+#     current_coordinates = models.CharField(max_length=255, default="0.0, 0.0")
+#     previous_town = models.CharField(max_length=255, default='...')
+#
+#     def __str__(self):
+#         return f"{self.tracking_number} ---> {self.starting_town} - {self.delivery_town}"
+#
